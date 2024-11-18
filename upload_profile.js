9331479fe4cc1ed63a1099d8a39cac2eea55818e
JobTube <jobsjobtube@gmail.com>
2024-11-18 23:27:52 +0400

    restore api

--- upload_profile.js
+++ upload_profile.js
@@ -3,9 +3,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination:  (req, file, cb) => {
-        if (!fs.existsSync(`/data-files/${req.body.path}/`)){
-            fs.mkdirSync(`/data-files/${req.body.path}/`);
-        }
+        if (!fs.existsSync(`/data-files/${req.body.path}/`)) fs.mkdirSync(`/data-files/${req.body.path}/`);
         cb(null, `/data-files/${req.body.path}/`);
     },
     filename: (req, file, cb) => cb(null,  `profile.png`),
--- upload_record.js
+++ upload_record.js
@@ -5,17 +5,17 @@ const pool = require('./connection');
 const storage = multer.diskStorage({
     destination: async  (req, file, cb) => {
         try {
-            if (!fs.existsSync(`files/${req.body.path}/`)) fs.mkdirSync(`files/${req.body.path}/`);
+            if (!fs.existsSync(`/data-files/${req.body.path}/`)) fs.mkdirSync(`/data-files/${req.body.path}/`);
             await pool.query(
                 `INSERT INTO videos (employment, description, user_id) VALUES ($1, $2, $3);`,
                 [req.body.employment, req.body.description, req.body.user]
-            ).then(() => cb(null, `files/${req.body.path}/`));
+            ).then(() => cb(null, `/data-files/${req.body.path}/`));
         } catch (err) {
             console.error(err);
-            res.status(500).send('Internal Server Error');
+            res.status(500);
         }
     },
-    filename: (req, file, cb) => cb(null,  `${req.body.user}-${req.body.employment}`),
+    filename: (req, file, cb) => cb(null,  `${req.body.id}-${req.body.employment}`),
 });
 
 const upload = multer({ storage: storage });
