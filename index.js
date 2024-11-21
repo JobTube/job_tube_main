147b150ee954e6622edb1d450cd0b8f3504b7d1a
JobTube <jobsjobtube@gmail.com>
2024-11-22 01:12:46 +0400

    restore api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs');
-const multer = require('multer');
 const http = require('http');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -248,29 +247,7 @@ app.get('/user-video/:token/:file', (req, res) => {
     }
 });
 
-const storage = multer.diskStorage({
-    destination: async  (req, file, cb) => {
-        try {
-            const check = await pool.query(`SELECT COUNT(id) FROM videos WHERE employment='${req.body.user}${req.body.employment}' AND user_id = ${req.body.user}`);
-            if (!fs.existsSync(`/data-files/${req.body.path}/`)) fs.mkdirSync(`/data-files/${req.body.path}/`);
-            if (!check.rows.length){
-                await pool.query(
-                    `INSERT INTO videos (index, employment, description, types, user_id) VALUES ($1, $2, $3, $4, $5);`,
-                    [req.body.index, `${req.body.user}${req.body.employment}`, req.body.description, req.body.types, req.body.user]
-                );
-            }
-            cb(null, `/data-files/${req.body.path}/`);
-        } catch (err) {
-            console.error(err);
-            res.status(500);
-        }
-    },
-    filename: (req, file, cb) => cb(null,  `${req.body.user}${req.body.employment}.mp4`),
-});
-
-const upload = multer({ storage: storage });
-
-app.post('/video-record/', upload.single('file'), (req, res) => res.sendStatus( req.file ? 200 : 400));
+app.post('/video-record/', upload_record.single('file'), (req, res) => res.sendStatus( req.file ? 200 : 400));
 
 app.post('/add-profile/', upload_profile.single('file'), (req, res) => res.sendStatus( req.file ? 200 : 400));
 
--- upload_record.js
+++ upload_record.js
@@ -5,14 +5,14 @@ const pool = require('./connection');
 const storage = multer.diskStorage({
     destination: async  (req, file, cb) => {
         try {
-            const check = await pool.query(`SELECT COUNT(id) FROM videos WHERE employment='${req.body.user}${req.body.employment}'`);
+            // const check = await pool.query(`SELECT COUNT(id) FROM videos WHERE employment='${req.body.user}${req.body.employment}'`);
             if (!fs.existsSync(`/data-files/${req.body.path}/`)) fs.mkdirSync(`/data-files/${req.body.path}/`);
-            if (!check.rows.length){
+            // if (!check.rows.length){
                 await pool.query(
                     `INSERT INTO videos (index, employment, description, types, end_date, user_id) VALUES ($1, $2, $3, $4, $5, $6);`,
                     [req.body.index, `${req.body.user}-${req.body.employment}`, req.body.description, req.body.types, parseInt(req.body.index) == 1 ? req.body.end : null, req.body.user]
                 );
-            }
+            // }
             cb(null, `/data-files/${req.body.path}/`);
         } catch (err) {
             console.error(err);
