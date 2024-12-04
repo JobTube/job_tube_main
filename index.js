1f8c04f11f4eab30153ea09e2d331b0f43f29e31
JobTube <jobsjobtube@gmail.com>
2024-12-04 16:45:07 +0400

    restore api

--- index.js
+++ index.js
@@ -226,8 +226,9 @@ app.post('/add-user', async(req, res) => {
             sendMail = true;
         } else {
             if(
-                check.rows[0].index == parseInt(req.body.index)
-                && check.rows[0].username == req.body.username
+                // check.rows[0].index == parseInt(req.body.index)
+                // && 
+                check.rows[0].username == req.body.username
                 // && check.rows[0].password == generateMd5(`SET_USER_DATA_${req.body.password}`)
                 // && check.rows[0].employment == req.body.employment 
                 &&
