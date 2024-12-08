b25ae55a268c342f9ee7de88a5f89a165023cde1
JobTube <jobsjobtube@gmail.com>
2024-12-08 18:00:36 +0400

    restore api

--- upload_record.js
+++ upload_record.js
@@ -1,15 +1,15 @@
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 const multer = require('multer');
 const pool = require('./connection');
 const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: async  (req, file, cb) => {
-        const name = uuidv4();
+        // const name = uuidv4();
         try {
             await pool.query(
                 `INSERT INTO videos (index, name, description, countries, types, end_date, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7);`,
-                [req.body.index, name, req.body.description, req.body.countries, req.body.types, parseInt(req.body.index) == 1 ? req.body.end : null, req.body.user]
+                [req.body.index, req.body.name, req.body.description, req.body.countries, req.body.types, parseInt(req.body.index) == 1 ? req.body.end : null, req.body.user]
             );
             if (!fs.existsSync(`/data-files/${req.body.path}/`)) fs.mkdirSync(`/data-files/${req.body.path}/`);
             cb(null, `/data-files/${req.body.path}/`);
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
             res.status(500);
         }
     },
-    filename: (req, file, cb) => cb(null,  `${name}.mp4`),
+    filename: (req, file, cb) => cb(null,  `${req.body.name}.mp4`),
 });
 
 const upload = multer({ storage: storage });
