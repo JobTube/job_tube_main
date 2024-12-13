bf0f11191271324c12bb46aa323a6eafeccd7603
JobTube <jobsjobtube@gmail.com>
2024-12-13 12:33:47 +0400

    restore api

--- index.js
+++ index.js
@@ -261,11 +261,12 @@ app.post('/user-login', async(req, res) => {
 
 app.post('/user-update', async(req, res) => {
     try {
-        await pool.query(`UPDATE users SET username='${req.body.user}' 
-            ${ [null, NAN, ''].includes(req.body.password) ? `` : `, password='${generateMd5(`SET_USER_DATA_${req.body.password}`)}' ` }  
-            ${ [null, NAN, ''].includes(req.body.email) ? `` : `, email='${req.body.email}' ` } 
-            ${ [null, NAN, ''].includes(req.body.address) ? `` : `, address='${req.body.address}' ` } 
-             WHERE token='${req.body.token}';`)
+        const updates = [];
+        if([null, NAN, ''].includes(req.body.password)) updates.push(`, password='${generateMd5(`SET_USER_DATA_${req.body.password}`)}'`);
+        if([null, NAN, ''].includes(req.body.email)) updates.push(`, email='${req.body.email}'`);
+        if([null, NAN, ''].includes(req.body.address)) updates.push(`, address='${req.body.address}}'`);
+
+        await pool.query(`UPDATE users SET username='${req.body.user}' ${updates.join(' ')}  WHERE token='${req.body.token}';`)
         .then(() => {
             res.json({"name": "successful", "code": "0"});
         });
