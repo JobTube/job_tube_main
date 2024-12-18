0867679a18afe4d30d24b58738883ad46745fa85
JobTube <jobsjobtube@gmail.com>
2024-12-18 15:55:25 +0400

    restore api

--- index.js
+++ index.js
@@ -486,8 +486,10 @@ app.get('/admin', async(req, res) => {
     res.json(data);
 });
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:3000`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
 });
 /*
     git add .
